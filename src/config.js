c5a181817c26827a49334dbd65a21f869b105a17
dreamingOfSoy <dreamingofsoy@gmail.com>
2022-06-20 15:23:46 +0100

    resolved security issue

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-export const baseURL =
-  'https://api.edamam.com/api/recipes/v2?type=public&app_id=64090b3f&app_key=863ec5c1694689e9e7474e4a313d6ed5&health=vegan';
+export const baseURL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=64090b3f&app_key=${KEY}&health=vegan`;
 
 export const TIMEOUT_SEC = 1;
